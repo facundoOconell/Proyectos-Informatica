977309c027af466461afd12d523b345b82a3675e
facundoOconell <166182870+facundoOconell@users.noreply.github.com>
2025-09-02 06:56:46 -0300

    Delete cv online directory

--- cv online/script.js
+++
@@ -1,9 +0,0 @@
-// Navegación suave al hacer clic en el menú
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
